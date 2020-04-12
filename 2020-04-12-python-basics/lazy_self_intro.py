b02185cdee1aa681954b0f002c2eedc4635abdc4
Cheukting <cheukting.ho@gmail.com>
2020-04-12 14:30:53 +0100

    adding example code for Python Basics

---
+++ 2020-04-12-python-basics/lazy_self_intro.py
@@ -0,0 +1,5 @@
+name = 'Lais'
+pronoun = 'She/her'
+favourite = 'Chocolate'
+
+print(f"I am {name}, my pronoun is {pronoun}, and my favourite thing is {favourite}")
---
+++ 2020-04-12-python-basics/loan_calculator.py
@@ -0,0 +1,8 @@
+name = "Cheuk"
+amount = 10_000
+interest_rate = 0.2
+year = 3
+
+payback = amount * (1+0.2) ** 3
+
+print(f"{name} has to pay back {payback}")
