52c1190fcc856b0bd46939d233eb2487aadabcb4
Cheukting <cheukting.ho@gmail.com>
2020-08-02 16:28:30 +0100

    adding python docs

---
+++ 2020-08-03-python-docs/my_ops.py
@@ -0,0 +1,10 @@
+def my_func(x):
+    """This is take the square of a number
+    Parameters
+    ----------
+    x : int, first variable
+    Return
+    ------
+    int, the squre of x
+    """
+    return x*x
